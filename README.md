93bc59d35fe06af83191b67219a04740ccce5643
Alba Silvente Fuentes <36744484+Dawntraoz@users.noreply.github.com>
2021-07-31 19:51:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Nuxt Storyblok Boilerplate
+
+> To get better understanding of this project and how it works, read our [Add a headless CMS to Nuxt in 5 minutes](https://www.storyblok.com/tp/headless-cms-nuxtjs) article.
+
+## Resources
+
+Visit our technology [Nuxt Technology Hub](https://www.storyblok.com/tc/nuxtjs) for more tutorials and videos how to use Storyblok together with Vue.js and Nuxt.
+
+## Configuration Steps
+
+1. Create new Space in Storyblok
+2. Obtain Preview Token from the Settings of the Space.
+3. Replace the `accessToken` value in `nuxt.config.js` file with your Preview Token.
+4. Your Nuxt.js project can be started and will consume data from your Storyblok Space.
+
+> For more advanced article on how to build multilanguage website with Storyblok and Nuxt.js, check our [The Complete Guide to Build a Full Blown Multilanguage Website with Nuxt.js](https://www.storyblok.com/tp/nuxt-js-multilanguage-website-tutorial).
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/_.vue
@@ -0,0 +1,62 @@
+<template>
+  <page
+    v-if="story.content.component"
+    :key="story.content._uid"
+    :blok="story.content" />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      story: { content: {} }
+    }
+  },
+  mounted () {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge()
+
+      // Listen to Storyblok's Visual Editor event
+      storyblokInstance.on(['input', 'published', 'change'], (event) => {
+        if (event.action == 'input') {
+          if (event.story.id === this.story.id) {
+            this.story.content = event.story.content
+          }
+        } else {
+          this.$nuxt.$router.go({
+            path: this.$nuxt.$router.currentRoute,
+            force: true,
+          })
+        }
+      })
+    }, (error) => {
+      console.error(error)
+    })
+  },
+  asyncData (context) {
+    // We are getting only the draft version of the content in this example.
+    // In real world project you should ask for correct version of the content
+    // according to the environment you are deploying to.
+    // const version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+
+    const fullSlug = (context.route.path == '/' || context.route.path == '') ? 'home' : context.route.path
+    const version =
+          context.query._storyblok || context.isDev ? 'draft' : 'published'
+
+    // Load the JSON from the API - loadig the home content (index page)
+    return context.app.$storyapi.get(`cdn/stories/${fullSlug}`, {
+      version
+    }).then((res) => {
+      return res.data
+    }).catch((res) => {
+      if (!res.response) {
+        console.error(res)
+        context.error({ statusCode: 404, message: 'Failed to receive content form api' })
+      } else {
+        console.error(res.response.data)
+        context.error({ statusCode: res.response.status, message: res.response.data })
+      }
+    })
+  }
+}
+</script>
