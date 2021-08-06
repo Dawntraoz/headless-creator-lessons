c68ac12c9ff508321fb8d3025ac206b7ae3b076a
Dawntraoz <albasilventefu@gmail.com>
2021-08-06 17:51:11 +0200

    Add github repo link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="font-montserrat font-medium text-sm text-gray-900">
+  <div class="font-montserrat font-medium text-sm text-gray-900 flex flex-col min-h-screen">
     <app-header />
-    <Nuxt role="main" />
+    <Nuxt role="main" class="flex-auto" />
+    <footer class="container mx-auto p-4">
+      <p>
+        Check the code at the 
+        <a
+          href="https://github.com/Dawntraoz/headless-creator-lessons"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-emerald-700 underline"
+        >
+        github repo
+        </a>
+      </p>
+    </footer>
   </div>
 </template>
